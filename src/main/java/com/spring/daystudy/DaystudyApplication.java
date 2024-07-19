fc6e249b8232b1e3241c10ba29c5bd8831560565
psora <psora93@gmail.com>
2024-07-19 22:49:35 +0900

    CHORE:Set project

---
+++ src/main/java/com/spring/daystudy/DaystudyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.daystudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaystudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaystudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/daystudy/DaystudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.daystudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaystudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
